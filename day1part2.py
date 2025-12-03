e9d1e81122c43443b217a58369a31d7df9cf3529
Dirk Kok <the@dirkkok.nl>
2025-12-03 12:27:18 +0100

    day1 part2 (forgot to save part1 whoops)

---
+++ day1part2.py
@@ -0,0 +1,34 @@
+def sign(num):
+    if num == 0:
+        return 0
+    if num > 0:
+        return 1
+    if num < 0:
+        return -1
+
+turns = []
+
+with open("day1.txt") as file:
+    for line in file:
+        if not (line.startswith("R") or line.startswith("L")):
+            raise f"Invalid line: {line}"
+
+        direction = line.startswith("R")
+        amount = int(line[1:])
+        turns.append(amount * (1 if direction else -1))
+
+number = 50
+answer = 0
+for turn in turns:
+    movedFromZero = number == 0
+    for _ in range(abs(turn)):
+        number += sign(turn)
+
+        if number == 100:
+            number -= 100
+        if number == -1:
+            number += 100
+        if number == 0:
+            answer += 1
+
+print(answer)
